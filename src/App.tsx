5484b66d56ae21b9e021f8b64d08291ce6611975
Christian <Christian.Huff.DEV@gmail.com>
2022-08-08 17:23:32 +0000

    Start implementing wireframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
+import JobBoard from "./components/JobBoard";
+
 function App() {
-  return (
-    <div>
-      <header>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+  return <JobBoard />;
 }
 
 export default App;
