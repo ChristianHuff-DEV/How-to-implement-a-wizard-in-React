883ea237729cf23e099016345f38afa8f3f0bde6
Christian <Christian.Huff.DEV@gmail.com>
2022-08-08 19:30:30 +0000

    Clean up styling

--- src/components/JobBoard.tsx
+++ src/components/JobBoard.tsx
@@ -1,13 +1,11 @@
 const JobBoard = () => {
   return (
     <div>
-      <div className="container mx-auto mt-8 md:flex md:items-center md:justify-between">
+      <div className="border-2 rounded-xl p-4 border-indigo-500 container mx-auto mt-10 flex items-center justify-between">
         <div className="flex-1 min-w-0">
-          <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
-            Jobs
-          </h2>
+          <h2 className="text-2xl font-bold leading-7 text-gray-900">Jobs</h2>
         </div>
-        <div className="mt-4 flex md:mt-0 md:ml-4">
+        <div className="flex mt-0">
           <button
             type="button"
             className="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
