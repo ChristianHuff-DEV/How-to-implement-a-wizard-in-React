db8481741596cd50c7e3bab09178c5b362a3579c
Christian <Christian.Huff.DEV@gmail.com>
2022-08-09 17:41:50 +0000

    Add React router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import JobBoard from "./components/JobBoard";
 import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <NavigationBar />
       <Home />
       <JobBoard />
-    </div>
+    </BrowserRouter>
   );
 }
 
