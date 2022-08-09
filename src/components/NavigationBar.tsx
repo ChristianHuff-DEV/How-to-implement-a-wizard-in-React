0c3b67dfff92b6a39bb3f55208f16a908511f5b6
Christian <Christian.Huff.DEV@gmail.com>
2022-08-09 17:52:56 +0000

    Work on routing

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,21 +1,23 @@
+import { Link } from "react-router-dom";
+
 const NavigationBar = () => {
   return (
     <div className="px-2 border-transparent border-b-2 border-gray-300">
       <div className="relative flex justify-between h-16">
         <div className="flex-1 flex items-stretch justify-start">
           <div className="ml-6 flex space-x-8">
-            <a
-              href="/"
+            <Link
+              to="/"
               className="border-transparent text-gray-800 hover:border-indigo-600 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-lg font-medium"
             >
               Home
-            </a>
-            <a
-              href="/jobs"
+            </Link>
+            <Link
+              to="/jobs"
               className="border-transparent text-gray-800 hover:border-indigo-600 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-lg font-medium"
             >
               Jobs
-            </a>
+            </Link>
           </div>
         </div>
       </div>
