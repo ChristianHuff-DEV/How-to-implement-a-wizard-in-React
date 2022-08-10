1a87e30da2b4365ede81dbd8e0861fc83184cfed
Christian <Christian.Huff.DEV@gmail.com>
2022-08-10 16:05:06 +0000

    Create dedicated file to hold data

---
+++ src/data.ts
@@ -0,0 +1,12 @@
+let jobs = [
+	{
+		name: "Lindsay Walton",
+		title: "Front-end Developer",
+		email: "lindsay.walton@example.com",
+		role: "Member",
+	},
+];
+
+export function getJobs() {
+	return jobs;
+}
