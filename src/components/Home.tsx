92ebd9252a39b54b4996def8360b2ca1a86f6305
Christian <Christian.Huff.DEV@gmail.com>
2022-08-10 15:36:06 +0000

    Repalce a with Link

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div className="container mx-auto ">
@@ -7,12 +9,12 @@ const Home = () => {
             <h1 className="flex-grow pr-16 text-2xl font-medium title-font text-gray-900">
               Find your next dream job.
             </h1>
-            <a
-              href="/jobs"
+            <Link
+              to="/jobs"
               className="hover:cursor-pointer ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Go to Jobs
-            </a>
+            </Link>
           </div>
         </div>
       </section>
