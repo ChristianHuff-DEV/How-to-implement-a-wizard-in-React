17aea095aa06b7f0e112696b22321e161040fbb5
Christian <Christian.Huff.DEV@gmail.com>
2022-08-10 16:26:29 +0000

    Start adding the wizard

--- src/components/EmployeesOverview.tsx
+++ src/components/EmployeesOverview.tsx
@@ -1,13 +1,19 @@
 import EmployeesOverviewHeader from "./EmployeesOverviewHeader";
 import EmployeeList from "./EmployeeList";
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 
 const EmployeesOverview = () => {
+  const [isAddEmployeeFormOpen, setAddEmployeeFormOpen] =
+    useState<boolean>(false);
+
   return (
     <div className="container mx-auto ">
-      <EmployeesOverviewHeader onAddEmployee={() => console.log("onEmployee")} />
+      <EmployeesOverviewHeader
+        onAddEmployee={() => console.log("onEmployee")}
+      />
       <EmployeeList />
-			<Outlet/>
+      <Outlet />
     </div>
   );
 };
