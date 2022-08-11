3669e0a43ee07533e41858efff91ba9ea54f21eb
Christian <Christian.Huff.DEV@gmail.com>
2022-08-11 19:35:42 +0000

    Work on wizard progress indicator

--- src/components/wizard/AddEmployeeWizard.tsx
+++ src/components/wizard/AddEmployeeWizard.tsx
@@ -1,22 +1,30 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
 import Step1 from "./Step1";
 import Step2 from "./Step2";
 import StepResult from "./StepResult";
 
-const steps = [
+interface Step {
+  id: string;
+  name: string;
+  to: string;
+  element: JSX.Element;
+  status: "upcoming" | "current" | "complete";
+}
+
+const steps: Step[] = [
   {
     id: "Step 1",
     name: "Personal Details",
     to: "/employees/addEmployee/step1",
     element: <Step1 />,
-    status: "complete",
+    status: "upcoming",
   },
   {
     id: "Step 2",
     name: "Job Details",
     to: "/employees/addEmployee/step2",
     element: <Step2 />,
-    status: "current",
+    status: "upcoming",
   },
   {
     id: "Step 3",
@@ -33,60 +41,49 @@ const AddEmployeeWizard = () => {
       {/* The routes defined here will be rendered in the <Outlet /> in the component of the parent path. */}
       <Routes>
         {steps.map((step) => {
-          // Get the last part of the url
+          // Get the last part of the url. This is the relative url for the steps path
           const urlParts = step.to.split("/");
           const path = urlParts[urlParts.length - 1];
 
           return <Route key={path} path={path} element={step.element} />;
         })}
       </Routes>
-      <WizardNavigator />
+      <WizardProgress />
     </>
   );
 };
 
-function WizardNavigator() {
+function WizardProgress() {
+  const location = useLocation();
+  const { pathname } = location;
+
   return (
-    <nav aria-label="Progress">
-      <ol role="list" className="space-y-4 md:flex md:space-y-0 md:space-x-8">
-        {steps.map((step) => (
-          <li key={step.name} className="md:flex-1">
-            {step.status === "complete" ? (
+    <div>
+      <ol className="flex space-y-0 space-x-8">
+        {steps.map((step) => {
+          console.log(step.to + ":" + pathname);
+          console.log(pathname === step.to);
+
+          return (
+            <li key={step.name} className="flex-1">
               <Link
                 to={step.to}
-                className="group py-2 flex flex-col pl-0 pt-4 border-t-4 border-indigo-600 hover:border-indigo-800 "
+                className={`group py-2 flex flex-col pl-0 pt-4 border-t-4 ${
+                  pathname === step.to
+                    ? "border-indigo-400 hover:border-indigo-600 "
+                    : "border-indigo-600 hover:border-indigo-800"
+                } `}
               >
                 <span className="text-xs text-indigo-600 font-semibold tracking-wide uppercase group-hover:text-indigo-800">
                   {step.id}
                 </span>
                 <span className="text-sm font-medium">{step.name}</span>
               </Link>
-            ) : step.status === "current" ? (
-              <Link
-                to={step.to}
-                className="group py-2 flex flex-col pl-0 pt-4 border-t-4 border-indigo-400 hover:border-indigo-600 "
-                aria-current="step"
-              >
-                <span className="text-xs text-indigo-400 font-semibold tracking-wide uppercase">
-                  {step.id}
-                </span>
-                <span className="text-sm font-medium">{step.name}</span>
-              </Link>
-            ) : (
-              <Link
-                to={step.to}
-                className="group py-2 flex flex-col pl-0 pt-4 pb-0 border-t-4 border-gray-200 hover:border-gray-300"
-              >
-                <span className="text-xs text-gray-500 font-semibold tracking-wide uppercase group-hover:text-gray-700">
-                  {step.id}
-                </span>
-                <span className="text-sm font-medium">{step.name}</span>
-              </Link>
-            )}
-          </li>
-        ))}
+            </li>
+          );
+        })}
       </ol>
-    </nav>
+    </div>
   );
 }
 
