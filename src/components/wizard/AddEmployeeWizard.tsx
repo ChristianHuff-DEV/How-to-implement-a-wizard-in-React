516c88f1daa4ad00f21acf8bc057b16b8d65d2bf
Christian <Christian.Huff.DEV@gmail.com>
2022-08-11 17:49:30 +0000

    Work on wizard navigator

--- src/components/wizard/AddEmployeeWizard.tsx
+++ src/components/wizard/AddEmployeeWizard.tsx
@@ -4,9 +4,24 @@ import Step2 from "./Step2";
 import StepResult from "./StepResult";
 
 const steps = [
-  { id: "Step 1", name: "Personal Details", to: "/employees/addEmployee/step1", status: "complete" },
-  { id: "Step 2", name: "Job Details", to: "/employees/addEmployee/step2", status: "current" },
-  { id: "Step 3", name: "Summary", to: "/employees/addEmployee/summary", status: "upcoming" },
+  {
+    id: "Step 1",
+    name: "Personal Details",
+    to: "/employees/addEmployee/step1",
+    status: "complete",
+  },
+  {
+    id: "Step 2",
+    name: "Job Details",
+    to: "/employees/addEmployee/step2",
+    status: "current",
+  },
+  {
+    id: "Step 3",
+    name: "Summary",
+    to: "/employees/addEmployee/summary",
+    status: "upcoming",
+  },
 ];
 
 const AddEmployeeWizard = () => {
@@ -18,12 +33,12 @@ const AddEmployeeWizard = () => {
         <Route path="step2" element={<Step2 />} />
         <Route path="summary" element={<StepResult />} />
       </Routes>
-      <Example />
+      <WizardNavigator />
     </>
   );
 };
 
-function Example() {
+function WizardNavigator() {
   return (
     <nav aria-label="Progress">
       <ol role="list" className="space-y-4 md:flex md:space-y-0 md:space-x-8">
@@ -32,7 +47,7 @@ function Example() {
             {step.status === "complete" ? (
               <Link
                 to={step.to}
-                className="group pl-4 py-2 flex flex-col border-l-4 border-indigo-600 hover:border-indigo-800 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4"
+                className="group py-2 flex flex-col pl-0 pt-4 border-t-4 border-indigo-600 hover:border-indigo-800 "
               >
                 <span className="text-xs text-indigo-600 font-semibold tracking-wide uppercase group-hover:text-indigo-800">
                   {step.id}
@@ -42,7 +57,7 @@ function Example() {
             ) : step.status === "current" ? (
               <Link
                 to={step.to}
-                className="pl-4 py-2 flex flex-col border-l-4 border-indigo-400 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4"
+                className="group py-2 flex flex-col pl-0 pt-4 border-t-4 border-indigo-400 hover:border-indigo-600 "
                 aria-current="step"
               >
                 <span className="text-xs text-indigo-400 font-semibold tracking-wide uppercase">
@@ -53,7 +68,7 @@ function Example() {
             ) : (
               <Link
                 to={step.to}
-                className="group pl-4 py-2 flex flex-col border-l-4 border-gray-200 hover:border-gray-300 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4"
+                className="group py-2 flex flex-col pl-0 pt-4 pb-0 border-t-4 border-gray-200 hover:border-gray-300"
               >
                 <span className="text-xs text-gray-500 font-semibold tracking-wide uppercase group-hover:text-gray-700">
                   {step.id}
