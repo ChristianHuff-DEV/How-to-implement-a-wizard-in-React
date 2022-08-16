e8c8829978c08162405fad7719400b6a4d3a69ea
Christian <Christian.Huff.DEV@gmail.com>
2022-08-16 19:41:21 +0000

    Implement most basic example for form

--- src/components/wizard/Step1.tsx
+++ src/components/wizard/Step1.tsx
@@ -1,12 +1,40 @@
+import { SubmitHandler, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { StepProps, useAddEmployeeWizardState } from "./AddEmployeeWizard";
 
-const Step1 = (props: StepProps) => {
-  const state = useAddEmployeeWizardState((state) => state);
+/**
+ * Defines the values handles by the form
+ */
+interface Step1Inputs {
+  name: string;
+  email: string;
+}
 
+const Step1 = (props: StepProps) => {
   const navigate = useNavigate();
+  const state = useAddEmployeeWizardState((state) => state);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Step1Inputs>();
+
+  const onSubmit: SubmitHandler<Step1Inputs> = (data) => {
+    console.log("submitting");
+    if (props.nextStepPath) {
+      navigate(props.nextStepPath);
+    }
+
+    if (data.name) {
+      state.updateName(data.name);
+    }
+  };
+
   return (
-    <form className="space-y-8 divide-y divide-gray-200">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="space-y-8 divide-y divide-gray-200"
+    >
       <div className="pt-2 space-y-5">
         <div>
           <h3 className="text-lg leading-6 font-medium text-gray-900">
@@ -23,32 +51,20 @@ const Step1 = (props: StepProps) => {
             </label>
             <div className="mt-0 col-span-2">
               <input
+                {...register("name", {
+                  validate: {
+                    first: (v) => v !== "fail" || "fail if input is 'fail'",
+                  },
+                })}
                 type="text"
-                name="name"
-                id="name"
-                value={state.name}
-                onChange={(e) => state.updateName(e.currentTarget.value)}
+								defaultValue={state.name}
                 className="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 max-w-xs text-sm border-gray-300 rounded-md"
               />
-            </div>
-          </div>
-
-          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-            <label
-              htmlFor="email"
-              className="block text-md font-medium text-gray-700 sm:mt-px sm:pt-2"
-            >
-              Email
-            </label>
-            <div className="mt-0 col-span-2">
-              <input
-                type="email"
-                name="email"
-                id="email"
-                value={state.email}
-                onChange={(e) => state.updateEmail(e.currentTarget.value)}
-                className="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 max-w-xs text-sm border-gray-300 rounded-md"
-              />
+              {errors.name && (
+                <p className="mt-2 text-sm text-red-600" id="email-error">
+                  {errors.name.message}
+                </p>
+              )}
             </div>
           </div>
         </div>
@@ -67,7 +83,7 @@ const Step1 = (props: StepProps) => {
           </button>
           {props.nextStepPath && (
             <button
-              onClick={() => navigate(props.nextStepPath!)}
+              type="submit"
               className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Next
