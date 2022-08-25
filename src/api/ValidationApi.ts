ae0e425569860e64e0f8bb9684e8fc91676b61a6
Christian <Christian.Huff.DEV@gmail.com>
2022-08-25 19:42:36 +0000

    Refactor server-side validation

--- src/api/ValidationApi.ts
+++ src/api/ValidationApi.ts
@@ -1,18 +1,17 @@
 import { Step1FormInput } from "../components/wizard/Step1";
 
-type postDataResult<T> = {
+interface ValidationResult {
 	success: boolean;
-	errors?: { [P in keyof T]?: string[] };
-};
+	// The errors object consists out of the keys of the form input and the value being a string array
+	errors?: { [P in keyof Step1FormInput]?: string[] },
+}
 
-export const validate = ({ name }: Step1FormInput): Promise<postDataResult<Step1FormInput>> => {
-	return new Promise((resolve) => {
-		setTimeout(() => {
-			if (name === "Fred") {
-				resolve({ success: false, errors: { name: ["Name already taken"], }, });
-			} else {
-				resolve({ success: true });
-			}
-		}, 100);
-	});
-};
+export const validate = async (input: Step1FormInput): Promise<ValidationResult> => {
+
+	if (input.name === "Max") {
+		return { success: false, errors: { name: ["Name already taken"] } }
+	}
+
+	return { success: true }
+}
+	;
