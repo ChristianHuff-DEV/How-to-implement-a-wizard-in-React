ad8a1f6193eb8da6f9f5e441986570e99e95fd8b
Christian <Christian.Huff.DEV@gmail.com>
2022-08-28 16:52:45 +0000

    Show validation result on last step

--- src/components/wizard/StepResult.tsx
+++ src/components/wizard/StepResult.tsx
@@ -1,51 +1,67 @@
 import { FormEvent } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { validate, ValidationResult } from "../../api/ValidationApi";
 import TextInput from "../TextInput";
 import { useAddEmployeeWizardState } from "./AddEmployeeWizard";
+import { Step1FormInput } from "./Step1";
 
 const StepResult = () => {
   const navigate = useNavigate();
   const state = useAddEmployeeWizardState((state) => state);
+  const {
+    handleSubmit,
+    setError,
+    formState: { errors },
+	} = useForm<Step1FormInput>(
+		// Initialize using the state
+		{ defaultValues: { ...state } });
 
-  const onSubmit = (event: FormEvent) => {
-    event.preventDefault();
+  /**
+   * Map the errors received from the server to th
+   */
+  const mapErrors = (result: ValidationResult) => {
+    if (result.errors?.name) {
+      setError("name", { type: "server", message: result.errors.name });
+    }
+  };
+
+  const onSubmit: SubmitHandler<Step1FormInput> = async (data) => {
+		console.log(data)
+    const validationResult = await validate(data);
+		console.log(validationResult)
+    if (!validationResult.success && validationResult.errors) {
+      mapErrors(validationResult);
+    }
   };
 
   return (
-    <form className="container py-5" onSubmit={onSubmit}>
+    <form className="container py-5" onSubmit={handleSubmit(onSubmit)}>
       <div className="grid grid-cols-3 gap-4 items-start border-t border-gray-200 pt-5">
-        <TextInput
-          className="col-span-2"
-          name="name"
-          label="Name"
-          value={state.name}
-          error={state.errors.name}
-          readOnly
-        />
-        <TextInput
-          className="col-span-2"
-          name="email"
-          label="Email"
-          value={state.email}
-          error={state.errors.email}
-          readOnly
-        />
-        <TextInput
-          className="col-span-2"
-          name="title"
-          label="Title"
-          value={state.title}
-          error={state.errors.title}
-          readOnly
-        />
-        <TextInput
-          className="col-span-2"
-          name="role"
-          label="Role"
-          value={state.role}
-          error={state.errors.role}
-          readOnly
-        />
+        <div className="col-span-2">
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-700 mt-px pt-2"
+          >
+            Name
+          </label>
+          <div className="mt-0">
+            <div className="max-w-lg flex rounded-md shadow-sm">
+              <input
+                className="flex-1 block w-full text-gray-500 focus:ring-gray-300 focus:border-gray-300 min-w-0 rounded-md sm:text-sm border-gray-300"
+                type="text"
+                name="name"
+                value={state.name}
+                readOnly
+              />
+            </div>
+            {errors.name && (
+              <p className="mt-2 text-sm text-red-600" id="email-error">
+                {errors.name.message}
+              </p>
+            )}
+          </div>
+        </div>
       </div>
       <div className="py-5">
         <div className="flex justify-end">
