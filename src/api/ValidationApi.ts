3aee6d3bf65c822aef23c8f0270cf65e8e4d7f1e
Christian <Christian.Huff.DEV@gmail.com>
2022-08-31 16:39:01 +0000

    Add dummy validation for all fields

--- src/api/ValidationApi.ts
+++ src/api/ValidationApi.ts
@@ -3,14 +3,29 @@ import { AddEmployeeWizardInput } from "../components/wizard/AddEmployeeWizard";
 export interface ValidationResult {
 	isValid: boolean;
 	// The errors object consists out of the keys of the form input and the value being a string array
-	errors?: { [P in keyof AddEmployeeWizardInput]?: string },
+	errors: { [P in keyof AddEmployeeWizardInput]?: string },
 }
 
 export const validate = async (input: AddEmployeeWizardInput): Promise<ValidationResult> => {
 
-	if (input.name === "Tom" || input.name === "tom") {
-		return { isValid: false, errors: { name: "Name already taken" } }
+	let validationResult: ValidationResult = { isValid: true, errors: {} }
+
+	if (input.name === "fail") {
+		validationResult.isValid = false
+		validationResult.errors.name = "Server side validation of the name failed"
+	}
+	if (input.email === "fail") {
+		validationResult.isValid = false
+		validationResult.errors.email = "Server side validation of the email failed"
+	}
+	if (input.title === "fail") {
+		validationResult.isValid = false
+		validationResult.errors.title = "Server side validation of the title failed"
+	}
+	if (input.role === "fail") {
+		validationResult.isValid = false
+		validationResult.errors.role = "Server side validation of the role failed"
 	}
 
-	return { isValid: true }
+	return validationResult
 }
